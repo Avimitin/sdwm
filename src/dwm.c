12ea0cb4f07a2f23f65f2507b46916296bed9850
avimitin <avimitin@gmail.com>
2021-08-02 19:01:50 +0800

    Fixed status bar shrinked problem

--- src/dwm.c
+++ src/dwm.c
@@ -828,7 +828,7 @@ drawbar(Monitor *m)
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
+		tw = TEXTW(stext) + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2, stext, 0);
 	}
 
