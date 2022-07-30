bb1c3aa49e60d04e487641f6e2373786afadf93e
Avimitin <avimitin@gmail.com>
2022-07-30 13:05:34 +0800

    fix: fix invalid layout switch
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/config.def.h
+++ src/config.def.h
@@ -106,7 +106,7 @@ static const char* monocle_windows_count_tags[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"rofi", "--no-lazy-grab", "-show", "drun", "-theme", "blurry",  NULL };
 static const char *scscmd[] = { "flameshot", "gui",  NULL };
-static const char *termcmd[]  = { "st", "-c", "simp-term", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 /* static const char *termcmd[]  = { "tabbed", "-d", "-r", "2", "st", "-w", "\"\"", NULL }; */
 
 /* changed all sh1marin to your name */
@@ -138,8 +138,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
